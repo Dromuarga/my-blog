ffe18523f736ad86ae7408c321b778f23cd1430e
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2023-09-13 23:49:37 +0200

    Use TailwindCSS to style the blog

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,12 +3,14 @@ import { PostMetaData } from "./PostMetaData";
 
 const PostPreview = (props: PostMetaData) => {
   return (
-    <div>
+    <div className="border border-slate-200 p-4 rounded-md shadow-md bg-white">
       <Link href={`/posts/${props.slug}`}>
-        <h2>{props.title}</h2>
+        <h2 className="font-bold text-violet-600 hover:underline">
+          {props.title}
+        </h2>
       </Link>
-      <p>{props.subtitle}</p>
-      <p>{props.date}</p>
+      <p className="text-sm text-slate-400">{props.date}</p>
+      <p className="text-slate-700">{props.subtitle}</p>
     </div>
   );
 };
